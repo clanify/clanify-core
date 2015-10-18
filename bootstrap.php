5ac9bade0a3a4b438d55793018a0d458f1e4d255
sebastianbrosch <contact@sebastianbrosch.de>
2015-10-18 11:10:34 +0200

    configuration files for phpunit.

---
+++ bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+//define the source directory of the project.
+define('PROJECT_SRC', __DIR__);
+
+//include the autoloader.
+require_once(PROJECT_SRC.'/vendor/autoload.php');
+require_once(PROJECT_SRC.'/src/Autoloader.php');
+
+//initialize the autoloader for the project source.
+$autoloader = new \Clanify\Autoloader();
+$autoloader->addNamespace('\Clanify', PROJECT_SRC.'/src');
