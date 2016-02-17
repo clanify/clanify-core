70554b69e11384373ff7b31c3666c74cad1dab10
sebastianbrosch <contact@sebastianbrosch.de>
2016-02-17 22:52:42 +0100

    added common specifications.

---
+++ src/Domain/Specification/Common/IsValidID.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Namespace for all common Specifications.
+ * @since 0.0.1-dev
+ */
+namespace Clanify\Domain\Specification\Common;
+
+use Clanify\Domain\Entity\Entity;
+use Clanify\Domain\Entity\IEntity;
+use Clanify\Domain\Specification\Specification;
+
+/**
+ * Class IsValidID
+ *
+ * @author Sebastian Brosch <contact@sebastianbrosch.de>
+ * @copyright 2016 Clanify
+ * @license GNU General Public License, version 3
+ * @package Clanify\Domain\Specification\Common
+ * @version 0.0.1-dev
+ */
+class IsValidID extends Specification
+{
+    /**
+     * Method to check if the Entity satisfies the Specification.
+     * @param IEntity $entity The Entity which will be checked.
+     * @return bool The state if the Entity satisfies the Specification.
+     * @since 0.0.1-dev
+     */
+    public function isSatisfiedBy(IEntity $entity)
+    {
+        //check if an Entity is available.
+        if ($entity instanceof Entity) {
+
+            //check if the id is valid.
+            if (preg_match('/^[0-9]+$/', $entity->id) > 0) {
+                return true;
+            }
+        }
+
+        //return the state.
+        return false;
+    }
+}
