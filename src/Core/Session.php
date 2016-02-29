62b13a8a417fb436041e1ac250573b7a82b1a3c7
sebastianbrosch <contact@sebastianbrosch.de>
2016-03-01 00:13:54 +0100

    added new session class to organize the session with database.

--- src/Core/Session.php
+++ src/Core/Session.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Namespace for all Entities of Clanify.
+ * Namespace for all core functions of Clanify.
  * @since 0.0.1-dev
  */
 namespace Clanify\Core;
@@ -9,45 +9,184 @@ namespace Clanify\Core;
  * Class Session
  *
  * @author Sebastian Brosch <contact@sebastianbrosch.de>
- * @copyright 2015 Clanify
+ * @copyright 2016 Clanify
  * @license GNU General Public License, version 3
- * @package Clanify\Domain\Entity
+ * @package Clanify\Core
  * @version 0.0.1-dev
  */
 class Session
 {
     /**
-     * Method to create a session.
+     * The PDO object to connect with the database.
+     * @since 0.0.1-dev
+     * @var \PDO|null
+     */
+    private $pdo = null;
+
+    /**
+     * The ID of the session.
+     * @since 0.0.1-dev
+     * @var string
+     */
+    public $id = '';
+
+    /**
+     * The content of the session (content of global session array).
+     * @since 0.0.1-dev
+     * @var string
+     */
+    public $content = '';
+
+    /**
+     * The seconds since the UNIX epoch (1970-01-01) of the session.
+     * @since 0.0.1-dev
+     * @var int
+     */
+    public $created = 0;
+
+    /**
+     * The ID of the User Entity which created the session.
+     * @since 0.0.1-dev
+     * @var int
+     */
+    public $user_id = 0;
+
+    /**
+     * Method to close the session.
+     * @return bool The state if the session could be closed.
      * @since 0.0.1-dev
      */
-    public static function create()
+    public function close()
     {
-        //set the session settings.
-        ini_set('session.use_cookies', 1);
-        ini_set('session.use_only_cookies', 1);
-        ini_set('session.use_trans_sid', 0);
-        ini_set('session.cookie_httponly', 1);
-
-        //set the session name.
-        session_name('clanify');
-
-        //start the session.
-        session_start();
-
-        //regenerate the session and delete the old one.
-        session_regenerate_id(true);
+        $this->pdo = null;
+        return true;
     }
 
     /**
-     * Method to destroy a session.
+     * Method to create the session and set the information.
+     * @param \PDO $pdo The PDO object to connect with database.
+     * @param int $user_id The ID of the User Entity.
      * @since 0.0.1-dev
      */
-    public static function destroy()
+    public function create(\PDO $pdo, $user_id = 0)
     {
-        //expire the session.
-        setcookie(session_name(), '', time() - 3600);
+        //set the information of the session.
+        $this->pdo = $pdo;
+        $this->user_id = $user_id;
 
-        //destroy the session.
-        session_destroy();
+        //override the session handler with the own.
+        session_set_save_handler(
+            array($this, "open"),
+            array($this, "close"),
+            array($this, "read"),
+            array($this, "write"),
+            array($this, "destroy"),
+            array($this, "gc")
+        );
+
+        //start the session.
+        session_start();
+    }
+
+    /**
+     * Method to destroy the session.
+     * @param string $id The ID of the session.
+     * @return bool The state if the session was destroyed.
+     * @since 0.0.1-dev
+     */
+    public function destroy($id)
+    {
+        //create and set the sql query.
+        $sql = 'DELETE FROM session WHERE id = :id';
+        $sth = $this->pdo->prepare($sql);
+
+        //bind the values to the query.
+        $sth->bindParam(':id', $id, \PDO::PARAM_INT);
+
+        //execute the query and return the state.
+        return $sth->execute();
+    }
+
+    /**
+     * Method which represent the garbage collector to destroy expired sessions.
+     * @param int $lifetime The lifetime of the session in seconds.
+     * @return bool The state if the garbage collector was successfull.
+     * @since 0.0.1-dev
+     */
+    public function gc($lifetime)
+    {
+        //create and set the sql query.
+        $sql = 'DELETE FROM session WHERE created < :expired';
+        $sth = $this->pdo->prepare($sql);
+
+        //get the expired time.
+        $expired = time() - $lifetime;
+
+        //bind the values to the query.
+        $sth->bindParam(':expired', $expired, \PDO::PARAM_INT);
+
+        //execute the query and return the state.
+        return $sth->execute();
+    }
+
+    /**
+     * Method to open the session.
+     * @return bool The state if the session could be opened.
+     * @since 0.0.1-dev
+     */
+    public function open()
+    {
+        return ($this->pdo instanceof \PDO);
+    }
+
+    /**
+     * Method to read the session from database.
+     * @param string $id The ID of the session which will be read.
+     * @return string The content of the session.
+     * @since 0.0.1-dev
+     */
+    public function read($id)
+    {
+        //create and set the sql query.
+        $sql = 'SELECT content FROM session WHERE id = :id';
+        $sth = $this->pdo->prepare($sql);
+
+        //bind the values to the query.
+        $sth->bindParam(':id', $id, \PDO::PARAM_STR);
+
+        //execute the query.
+        if ($sth->execute()) {
+
+            //get the row from database.
+            if ($row = $sth->fetch(\PDO::FETCH_ASSOC)) {
+                return $row['content'];
+            }
+        }
+
+        //return the default.
+        return '';
+    }
+
+    /**
+     * Method to write the session to the database.
+     * @param string $id The ID of the session.
+     * @param string $content The content of the session.
+     * @return bool The state if the session could be written.
+     * @since 0.0.1-dev
+     */
+    public function write($id, $content)
+    {
+        //create and set the sql query.
+        $sql = 'REPLACE INTO session (id, content, created, user_id) VALUES (:id, :content, :created, :user_id)';
+        $sth = $this->pdo->prepare($sql);
+
+        //bind the values to the query.
+        $sth->bindParam(':id', $id, \PDO::PARAM_STR);
+        $sth->bindParam(':created', time(), \PDO::PARAM_INT);
+        $sth->bindParam(':data', $content, \PDO::PARAM_STR);
+        $sth->bindParam(':user_id', $this->user_id, \PDO::PARAM_INT);
+
+        //execute the query and return the state.
+        return $sth->execute();
     }
 }
